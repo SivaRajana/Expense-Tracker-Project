c52540750aea11e2cd07ce8f42f2c4acd6936e25
Siva <sivathebatman@gmail.com>
2022-03-31 11:36:12 +0530

    Initial commit to Expense Item project

---
+++ src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,24 @@
+import './ExpenseDate.css';
+
+const ExpenseDate = (props) => {
+    const month = props.date.toLocaleString('en-US',{month:'long'});
+    const day = props.date.getFullYear();
+    const year = props.date.getDate();
+    
+    return (
+        <div className='expense-date'>
+            <div className='expense-date__month'>
+                {month}
+            </div>
+            <div className='expense-date__year'>
+                {year}
+            </div>
+            <div className='expense-date__day'>
+                {day}
+            </div>
+        </div>
+    )
+
+}
+
+export default ExpenseDate;
