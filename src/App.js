e5ec813247b3aca7aff35ac7f113d9b270b4ec02
Siva <sivathebatman@gmail.com>
2022-04-06 01:08:59 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expense from './components/Expenses/Expense';
 
 const App = () => {
-    
+
   const dummyData = [
     {
       id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
+      title: 'Udemy React Course',
+      amount: 550,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e2', title: 'New House', amount: 340000, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
+      title: 'Coursera Python',
+      amount: 4500,
+      date: new Date(2022, 2, 28),
     },
     {
       id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
+      title: ' DELL Laptop',
+      amount: 45000,
+      date: new Date(2022, 5, 12),
     },
   ];
   const [expensesList, setExpenseList] = useState(dummyData);
 
   // getting the newExpense from from NewExpense component as a parameter while calling onExpenseData prop which is a function
   const expenseDataHandler = (newExpense) => {
-    setExpenseList(prevSnapshot => [newExpense,...prevSnapshot]);
+    setExpenseList(prevSnapshot => [newExpense, ...prevSnapshot]);
   }
-  
- return (
+
+  return (
     <div>
-      <NewExpense onExpenseData = {expenseDataHandler}/> {/* passing function as prop here */}
-      <Expense expenses={expensesList}/>
+      <NewExpense onExpenseData={expenseDataHandler} /> {/* passing function as prop here */}
+      <Expense expenses={expensesList} />
     </div>
   );
 }
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css'
 
 const ExpenseFrom = (props) => {
@@ -6,7 +6,7 @@ const ExpenseFrom = (props) => {
     const [titleInput, setTitleInput] = useState("");
     const [amountInput, setAmountInput] = useState("");
     const [dateInput, setDateInput] = useState("");
-    
+
     const titleChangeHandler = (event) => {
         setTitleInput(event.target.value);
     }
@@ -23,18 +23,18 @@ const ExpenseFrom = (props) => {
         event.preventDefault();
         const userInputData = {
             title: titleInput,
-            amount: amountInput,
+            amount: parseInt(amountInput),
             date: new Date(dateInput)
         }
         props.onNewExpense(userInputData); //Calling function which is defined in parent(new Expense)
-        
+
         setTitleInput("");
         setAmountInput("");
         setDateInput("");
         setFullFormNeed(false); //after submitting form should shrink 
     }
 
-    const [fullFormNeed,setFullFormNeed] = useState(false);
+    const [fullFormNeed, setFullFormNeed] = useState(false);
 
     const cancelHandler = () => {
         setFullFormNeed(false);
@@ -44,30 +44,30 @@ const ExpenseFrom = (props) => {
     };
 
     const displayFullForm = () => setFullFormNeed(true);
-    
+
     const fullForm = <div>
-                        <div className="new-expense__controls">
-                            <div className="new-expense__controls">
-                                <label>Title</label>
-                                <input type="text" placeholder="Add Title" value={titleInput} onChange={titleChangeHandler}/>
-                            </div>
-                            <div className="new-expense__controls">
-                                <label>Amount</label>
-                                <input type="number" placeholder="Amount" min="0.01" step="0.01" value={amountInput} onChange={amountChangeHandler}/>
-                            </div>
-                            <div className="new-expense__controls">
-                                <label>Date</label>
-                                <input type="date" min="2019-01-01" max="2022-12-31" value={dateInput} onChange={dateChangeHandler}/>
-                            </div>
-                        </div>
-                        <div>
-                            <button className='new-expense__actions' onClick={cancelHandler}>Cancel</button>
-                            <button className='new-expense__actions' type='submit'>Add Expense</button>
-                        </div>
-                     </div>
+        <div className="new-expense__controls">
+            <div className="new-expense__controls">
+                <label>Title</label>
+                <input type="text" placeholder="Add Title" value={titleInput} onChange={titleChangeHandler} />
+            </div>
+            <div className="new-expense__controls">
+                <label>Amount</label>
+                <input type="number" placeholder="Amount" min="0.01" step="0.01" value={amountInput} onChange={amountChangeHandler} />
+            </div>
+            <div className="new-expense__controls">
+                <label>Date</label>
+                <input type="date" min="2019-01-01" max="2022-12-31" value={dateInput} onChange={dateChangeHandler} />
+            </div>
+        </div>
+        <div>
+            <button className='new-expense__actions' onClick={cancelHandler}>Cancel</button>
+            <button className='new-expense__actions' type='submit'>Add Expense</button>
+        </div>
+    </div>
 
     const partialForm = <button className='new-expense__actions' onClick={displayFullForm}>Add Expense</button>
-            
+
     return (
         <form onSubmit={onSubmitHandler}>
             {fullFormNeed === false && partialForm}
